d67a026c020ffdc391347611a8368a6f2c712335
Ramiro Massafra <ramiromassafra225@gmail.com>
2025-06-24 17:50:07 -0300

    symfony 24/06/2025

--- twig/eje5.php
+++ twig/eje5.php
@@ -2,5 +2,5 @@
        require_once './vendor/autoload.php';
        $loader= new \Twig\Loader\FilesystemLoader('.\templates');
         $twig= new \Twig\Environment($loader);
-        echo $twig->render('.\eje5.html.twig', ['nombre' => $_GET['nombre'],['numero' => $_GET['numero']]]);
+        echo $twig->render('eje5.html.twig', ['nombre' => $_GET['nombre'],'numero' => $_GET['numero']]);
        ?> 
