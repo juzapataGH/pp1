8353ef850061bb366473549e49c8d42707ca3898
julian zapata <julianzapata1264@gmail.com>
2025-08-12 19:15:56 -0300

    -12/08/2025

--- carrito/src/DataFixtures/ProductoFixtures.php
+++ carrito/src/DataFixtures/ProductoFixtures.php
@@ -9,10 +9,10 @@ class ProductoFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for($i=1;$i<10;$i++){
+        for($i=1;$i<=10;$i++){
         $producto = new Producto();
         $producto->setNombre('Producto'.$i);
-        $producto->setDescripcion('dDSJADJADHJASHDJDHSAJDHSDJDSH');
+        $producto->setDescripcion('dDS JADJADHJASHDJ HSAJD HSDJDSH');
         $producto->setPrecio(rand(10,100));
         $producto->setImagen('images/producto'.$i.'.jpg');
         $manager->persist($producto);
